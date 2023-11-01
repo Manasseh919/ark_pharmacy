be5a8957b9cf37d46eba913e17f5855446593fc4
Manasseh919 <69004938+Manasseh919@users.noreply.github.com>
2023-11-01 17:08:52 +0000

    routes and backend setup done with node js

--- server/index.js
+++ server/index.js
@@ -1,11 +1,18 @@
 // console.log("hello world");
 
 const express = require("express");
+const authRouter = require("./routes/auth")
 const PORT = 4000;
 
 const app = express();
 
+//middleware
+app.use(authRouter);
+
+
 //creating api
-app.listen(PORT, "0.0.0.0", () => {
-  console.log(`Connected at port ${PORT}` );
+
+//GET,PUT,POST,DELETE,UPDate
+app.listen(PORT, () => {
+  console.log(`Connected at port ${PORT}`);
 });
