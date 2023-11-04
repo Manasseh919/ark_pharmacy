2f7e69995185c01cee621bb724fc9d2679cbeb82
Manasseh919 <69004938+Manasseh919@users.noreply.github.com>
2023-11-04 16:53:20 +0000

    persisting state complete

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:ark_pharmacy/constants/global_variables.dart';
 import 'package:ark_pharmacy/features/auth/screens/auth_screen.dart';
+import 'package:ark_pharmacy/features/auth/services/auth_service.dart';
+import 'package:ark_pharmacy/features/home/screens/home_screen.dart';
 import 'package:ark_pharmacy/providers/user_provider.dart';
 import 'package:ark_pharmacy/router.dart';
 import 'package:flutter/material.dart';
@@ -13,24 +15,39 @@ void main() {
   ], child: const MyApp()));
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  final AuthService authService = AuthService();
+
+  @override
+  void initState() {
+    super.initState();
+    authService.getUserData(context);
+  }
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Arkesel Pharmacy',
-      theme: ThemeData(
-        scaffoldBackgroundColor: GlobalVariables.backgroundColor,
-        colorScheme: ColorScheme.light(primary: GlobalVariables.secondaryColor),
-        appBarTheme: const AppBarTheme(
-          elevation: 0,
-          iconTheme: IconThemeData(color: Colors.black),
+        title: 'Arkesel Pharmacy',
+        theme: ThemeData(
+          scaffoldBackgroundColor: GlobalVariables.backgroundColor,
+          colorScheme:
+              ColorScheme.light(primary: GlobalVariables.secondaryColor),
+          appBarTheme: const AppBarTheme(
+            elevation: 0,
+            iconTheme: IconThemeData(color: Colors.black),
+          ),
         ),
-      ),
-      onGenerateRoute: (settings) => generateRoute(settings),
-      home: const AuthScreen(),
-    );
+        onGenerateRoute: (settings) => generateRoute(settings),
+        home: Provider.of<UserProvider>(context).user.token.isNotEmpty
+            ? const HomeScreen()
+            : const AuthScreen());
   }
 }
