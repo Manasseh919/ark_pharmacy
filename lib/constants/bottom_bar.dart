f3e4709619a83ce088a2dd778c6b9ae6211ae8e4
Manasseh919 <69004938+Manasseh919@users.noreply.github.com>
2023-11-06 08:34:26 +0000

    cart icons done with badges package

--- lib/constants/bottom_bar.dart
+++ lib/constants/bottom_bar.dart
@@ -1,5 +1,7 @@
 import 'package:ark_pharmacy/constants/global_variables.dart';
+import 'package:ark_pharmacy/features/home/screens/home_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:badges/badges.dart' as badges;
 
 class BottomBar extends StatefulWidget {
   static const String routeName = '/actual-home';
@@ -13,15 +15,34 @@ class _BottomBarState extends State<BottomBar> {
   int _page = 0;
   double bottomBarWidth = 42;
   double bottomBarBorderWidth = 5;
+
+  List<Widget> pages = [
+    const HomeScreen(),
+    const Center(
+      child: Text('Account Page'),
+    ),
+    const Center(
+      child: Text('Cart Page'),
+    ),
+  ];
+
+  void updatePage(int page) {
+    setState(() {
+      _page = page;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      body: pages[_page],
       bottomNavigationBar: BottomNavigationBar(
         currentIndex: _page,
         selectedItemColor: GlobalVariables.selectedNavBarColor,
         unselectedItemColor: GlobalVariables.unselectedNavBarColor,
         backgroundColor: GlobalVariables.backgroundColor,
         iconSize: 28,
+        onTap: updatePage,
         items: [
           BottomNavigationBarItem(
             icon: Container(
@@ -41,40 +62,43 @@ class _BottomBarState extends State<BottomBar> {
             label: "",
           ),
           BottomNavigationBarItem(
-            icon: Container(
-              width: bottomBarWidth,
-              decoration: BoxDecoration(
-                border: Border(
-                  top: BorderSide(
-                    color: _page == 1
-                        ? GlobalVariables.selectedNavBarColor
-                        : GlobalVariables.backgroundColor,
-                    width: bottomBarBorderWidth,
+              icon: Container(
+                width: bottomBarWidth,
+                decoration: BoxDecoration(
+                  border: Border(
+                    top: BorderSide(
+                      color: _page == 1
+                          ? GlobalVariables.selectedNavBarColor
+                          : GlobalVariables.backgroundColor,
+                      width: bottomBarBorderWidth,
+                    ),
                   ),
                 ),
+                child: const Icon(Icons.person_outline_outlined),
               ),
-              child: const Icon(Icons.person_outline_outlined),
-            ),
-            label: ""
-          ),
+              label: ""),
           BottomNavigationBarItem(
-            icon: Container(
-              width: bottomBarWidth,
-              decoration: BoxDecoration(
-                border: Border(
-                  top: BorderSide(
-                    color: _page == 2
-                        ? GlobalVariables.selectedNavBarColor
-                        : GlobalVariables.backgroundColor,
-                    width: bottomBarBorderWidth,
+              icon: Container(
+                width: bottomBarWidth,
+                decoration: BoxDecoration(
+                  border: Border(
+                    top: BorderSide(
+                      color: _page == 2
+                          ? GlobalVariables.selectedNavBarColor
+                          : GlobalVariables.backgroundColor,
+                      width: bottomBarBorderWidth,
+                    ),
                   ),
                 ),
+                child: badges.Badge(
+                  badgeContent: const Text('2'),
+                  badgeStyle: badges.BadgeStyle(
+                    badgeColor: Colors.white,
+                  ),
+                  child: const Icon(Icons.shopping_bag_outlined),
+                ),
               ),
-              child: const Icon(Icons.percent),
-            ),
-            label: ""
-          ),
-
+              label: ""),
         ],
       ),
     );
