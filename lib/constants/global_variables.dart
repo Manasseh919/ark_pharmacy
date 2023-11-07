8fed0b7ea3a56d55c94d2c23981f3dda087a6444
Manasseh919 <69004938+Manasseh919@users.noreply.github.com>
2023-11-07 11:10:21 +0000

    home Ui complete

--- lib/constants/global_variables.dart
+++ lib/constants/global_variables.dart
@@ -20,11 +20,11 @@ class GlobalVariables {
 
   // STATIC IMAGES
   static const List<String> carouselImages = [
-    'https://images-eu.ssl-images-amazon.com/images/G/31/img21/Wireless/WLA/TS/D37847648_Accessories_savingdays_Jan22_Cat_PC_1500.jpg',
-    'https://images-eu.ssl-images-amazon.com/images/G/31/img2021/Vday/bwl/English.jpg',
-    'https://images-eu.ssl-images-amazon.com/images/G/31/img22/Wireless/AdvantagePrime/BAU/14thJan/D37196025_IN_WL_AdvantageJustforPrime_Jan_Mob_ingress-banner_1242x450.jpg',
-    'https://images-na.ssl-images-amazon.com/images/G/31/Symbol/2020/00NEW/1242_450Banners/PL31_copy._CB432483346_.jpg',
-    'https://images-na.ssl-images-amazon.com/images/G/31/img21/shoes/September/SSW/pc-header._CB641971330_.jpg',
+    'https://www.pilldoctor.com.gh/wp-content/uploads/2021/02/bnr-img1.png',
+    'https://newcrystalhealth.org/obm/wp-content/uploads/2019/09/5-1.jpg',
+    'https://newcrystalhealth.org/obm/wp-content/uploads/2019/09/Slide-2-Online-Pharmacy.jpg',
+    'https://addpharma4u.com/wp-content/uploads/2021/05/WEN-1.png',
+    'https://addpharma4u.com/wp-content/uploads/2020/12/h1-new03.png',
   ];
 
   static const List<Map<String, String>> categoryImages = [
