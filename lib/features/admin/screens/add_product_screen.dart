eafdce39e0fc1f2b538548bf5966813ead9b5878
Manasseh919 <69004938+Manasseh919@users.noreply.github.com>
2023-11-08 10:22:28 +0000

    add products screen UI complete

--- lib/features/admin/screens/add_product_screen.dart
+++ lib/features/admin/screens/add_product_screen.dart
@@ -1,4 +1,7 @@
+import 'package:ark_pharmacy/common/widgets/custom_button.dart';
+import 'package:ark_pharmacy/common/widgets/custom_textfield.dart';
 import 'package:ark_pharmacy/constants/global_variables.dart';
+import 'package:dotted_border/dotted_border.dart';
 import 'package:flutter/material.dart';
 
 class AddProductScreen extends StatefulWidget {
@@ -10,6 +13,30 @@ class AddProductScreen extends StatefulWidget {
 }
 
 class _AddProductScreenState extends State<AddProductScreen> {
+  TextEditingController productNameController = TextEditingController();
+  TextEditingController descriptionController = TextEditingController();
+  TextEditingController quantityController = TextEditingController();
+  TextEditingController priceController = TextEditingController();
+
+  String category = 'Tools';
+
+  @override
+  void dispose() {
+    super.dispose();
+    productNameController.dispose();
+    descriptionController.dispose();
+    quantityController.dispose();
+    priceController.dispose();
+  }
+
+  List<String> productCategories = [
+    'Tools',
+    'Essentials',
+    'Mother',
+    'Sex & Wellness',
+    'Baby'
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -21,22 +48,98 @@ class _AddProductScreenState extends State<AddProductScreen> {
               gradient: GlobalVariables.appBarGradient,
             ),
           ),
-          title: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Container(
-                alignment: Alignment.topLeft,
-                child: Text(
-                  "Ark_Pharmacy",
-                  style: TextStyle(
-                      color: Colors.white, fontWeight: FontWeight.bold),
+          title: Text("Add to Products"),
+        ),
+      ),
+      body: SingleChildScrollView(
+        child: Form(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 10.0),
+            child: Column(
+              children: [
+                const SizedBox(
+                  height: 20,
                 ),
-              ),
-              Text(
-                "Admin",
-                style: TextStyle(fontWeight: FontWeight.bold),
-              )
-            ],
+                DottedBorder(
+                  borderType: BorderType.RRect,
+                  radius: Radius.circular(10),
+                  dashPattern: [10, 4],
+                  strokeCap: StrokeCap.round,
+                  child: Container(
+                    width: double.infinity,
+                    height: 150,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        const Icon(
+                          Icons.folder_open,
+                          size: 40,
+                        ),
+                        const SizedBox(
+                          height: 15,
+                        ),
+                        Text(
+                          "Select Product Images",
+                          style:
+                              TextStyle(fontSize: 15, color: Colors.grey[400]),
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+                const SizedBox(
+                  height: 30,
+                ),
+                CustomTextField(
+                    controller: productNameController,
+                    hintText: "Product Name"),
+                const SizedBox(
+                  height: 10,
+                ),
+                CustomTextField(
+                  controller: descriptionController,
+                  hintText: "Description",
+                  maxLines: 7,
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                CustomTextField(controller: priceController, hintText: "Price"),
+                const SizedBox(
+                  height: 10,
+                ),
+                CustomTextField(
+                    controller: quantityController, hintText: "Quantity"),
+                const SizedBox(
+                  height: 10,
+                ),
+                SizedBox(
+                  width: double.infinity,
+                  child: DropdownButton(
+                    items: productCategories.map((String item) {
+                      return DropdownMenuItem(
+                        value: item,
+                        child: Text(item),
+                      );
+                    }).toList(),
+                    value: category,
+                    icon: Icon(Icons.keyboard_arrow_down),
+                    onChanged: (String? newVal) {
+                      setState(() {
+                        category = newVal!;
+                      });
+                    },
+                  ),
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                CustomButton(text: "Sell", onTap: () {})
+              ],
+            ),
           ),
         ),
       ),
