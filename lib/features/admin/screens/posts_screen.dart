991fe7baba7162e251fc3e43b7eed82975ab6830
Manasseh919 <69004938+Manasseh919@users.noreply.github.com>
2023-11-13 09:39:43 +0000

    displaying all products on admin screen and api done

--- lib/features/admin/screens/posts_screen.dart
+++ lib/features/admin/screens/posts_screen.dart
@@ -1,4 +1,8 @@
+import 'package:ark_pharmacy/common/widgets/loader.dart';
+import 'package:ark_pharmacy/features/account/widgets/single_product.dart';
 import 'package:ark_pharmacy/features/admin/screens/add_product_screen.dart';
+import 'package:ark_pharmacy/features/admin/services/admin_services.dart';
+import 'package:ark_pharmacy/models/product.dart';
 import 'package:flutter/material.dart';
 
 class PostsScreen extends StatefulWidget {
@@ -9,22 +13,67 @@ class PostsScreen extends StatefulWidget {
 }
 
 class _PostsScreenState extends State<PostsScreen> {
+  List<Product>? products;
+  final AdminServices adminServices = AdminServices();
+
+  @override
+  void initState() {
+    super.initState();
+    fetchAllProducts();
+  }
+
+  fetchAllProducts() async {
+   products = await adminServices.fetchAllProducts(context);
+    setState(() {});
+  }
+
   void navigateToAddProduct() {
     Navigator.pushNamed(context, AddProductScreen.routeName);
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Text("products"),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: navigateToAddProduct,
-        tooltip: "Add a product",
-        child: Icon(Icons.add),
-      ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
-    );
+    return products == null
+        ? const Loader()
+        : Scaffold(
+            body:  GridView.builder(
+              itemCount: products!.length,
+              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2),
+              itemBuilder: (context, index) {
+                final productData = products![index];
+                return Column(
+                  children: [
+                    SizedBox(
+                      height: 140,
+                      child: SingleProduct(
+                        image: productData.images[0],
+                      ),
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        Expanded(
+                          child: Text(
+                            productData.name,
+                            overflow: TextOverflow.ellipsis,
+                            maxLines: 2,
+                          ),
+                        ),
+                        IconButton(
+                          onPressed: (){},
+                          icon: const Icon(
+                            Icons.delete_outline,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                );
+              },
+            ),
+            floatingActionButtonLocation:
+                FloatingActionButtonLocation.centerFloat,
+          );
   }
 }
